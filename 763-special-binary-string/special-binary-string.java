defe802aca1272c475b549109fc6a6ed8c74f2ba
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-02-20 17:58:34 +0530

    Time: 3 ms (51.42%) | Memory: 42.9 MB (93.13%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> list = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') count++;
+            else count--;
+            if (count == 0) {
+                String inner = s.substring(start + 1, i);
+                String processed = makeLargestSpecial(inner);
+                list.add("1" + processed + "0");
+                start = i + 1;
+            }
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        String result = "";
+        for (String str : list)
+            result += str;
+        return result;
+    }
+}
