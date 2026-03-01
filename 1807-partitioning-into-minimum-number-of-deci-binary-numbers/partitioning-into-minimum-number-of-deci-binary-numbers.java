1f7ae5d7df3bbbb94dddff0751a3c3ef18035b87
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-03-01 12:48:17 +0530

    Time: 7 ms (34.72%) | Memory: 47.5 MB (86.42%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minPartitions(String n) {
-        int maxDigit = 1;
+        int maxDigit = 0;
         for (int i = 0; i < n.length(); i++) {
             int digit = n.charAt(i) - '0';
             maxDigit = Math.max(maxDigit, digit);
