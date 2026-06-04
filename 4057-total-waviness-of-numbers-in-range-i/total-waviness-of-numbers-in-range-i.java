ff973856aaf1a08bd7e3b7035931cd114b5452a4
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-06-04 12:07:05 +0530

    Time: 31 ms (23.79%) | Memory: 46.3 MB (67.29%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        int total = 0;
+        for (int num = num1; num <= num2; num++){
+            total += waviness(num);
+        }
+        return total;
+    }
+    private int waviness(int num){
+        String s = String.valueOf(num);
+        if (s.length() < 3){
+            return 0;
+        }
+        int count = 0;
+        for (int i = 1; i < s.length() - 1; i++){
+            char left = s.charAt(i - 1);
+            char mid = s.charAt(i);
+            char right = s.charAt(i + 1);
+            if ((mid > left && mid > right) || (mid < left && mid < right)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
