2f1731600bdbc771d8d272b951ee9fa51841af9d
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-06-26 13:13:39 +0530

    Time: 14 ms (67.21%) | Memory: 87.8 MB (81.97%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    class BIT{
+        int[] bit;
+        BIT(int n){
+            bit=new int[n+8];
+        }
+        void update(int idx,int val){
+            while(idx<bit.length){
+                bit[idx]+=val;
+                idx+=idx&-idx;
+            }
+        }
+        int query(int idx){
+            int sum=0;
+            while(idx>0){
+                sum+=bit[idx];
+                idx-=idx&-idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums,int target){
+        int n=nums.length;
+        int offset=n+2;
+        BIT bit=new BIT(2*n+5);
+        int prefix=0;
+        long ans=0;
+        bit.update(offset,1);
+        for(int x:nums){
+            if(x==target) prefix++;
+            else prefix--;
+            ans+=bit.query(prefix+offset-1);
+            bit.update(prefix+offset,1);
+        }
+        return ans;
+    }
+}
