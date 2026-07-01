99729d123b239fd47b8d8e9f722b937209c07070
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-07-01 12:43:10 +0530

    Time: 188 ms (32.38%) | Memory: 127.8 MB (15.55%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n = grid.size();
+        int[][] dist = new int[n][n];
+        for (int[] row : dist) Arrays.fill(row, -1);
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    dist[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] d : dir) {
+                int x = cur[0] + d[0];
+                int y = cur[1] + d[1];
+                if (x >= 0 && y >= 0 && x < n && y < n && dist[x][y] == -1) {
+                    dist[x][y] = dist[cur[0]][cur[1]] + 1;
+                    q.offer(new int[]{x, y});
+                }
+            }
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        boolean[][] vis = new boolean[n][n];
+        pq.offer(new int[]{dist[0][0], 0, 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int safe = cur[0];
+            int r = cur[1];
+            int c = cur[2];
+            if (vis[r][c]) continue;
+            vis[r][c] = true;
+            if (r == n - 1 && c == n - 1) return safe;
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && !vis[nr][nc]) {
+                    pq.offer(new int[]{
+                        Math.min(safe, dist[nr][nc]),
+                        nr,
+                        nc
+                    });
+                }
+            }
+        }
+        return 0;
+    }
+}
