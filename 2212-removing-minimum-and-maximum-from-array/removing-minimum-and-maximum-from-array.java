237e809bc555cc789e046e0d8aae406478b66ed9
Sarthak Kalyani <sarthakkalyani007@gmail.com>
2026-08-30 18:56:03 +0530

    Time: 3 ms (61.82%) | Memory: 86.8 MB (40.00%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,42 +1,14 @@
-class Solution {
-    public int minimumDeletions(int[] nums) {
-        int n = nums.length;
-
-        // if there is only one element
-        if (n == 1)
-            return 1;
-
-        // variables to store index of smallest and largest number
-        int minIndex = 0;
-        int maxIndex = 0;
-
-        // find where minimum and maximum elements are
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < nums[minIndex]) {
-                minIndex = i;
-            }
-            if (nums[i] > nums[maxIndex]) {
-                maxIndex = i;
-            }
+class Solution{
+    public int minimumDeletions(int[] nums){
+        int n=nums.length;
+        int minIndex=0,maxIndex=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[minIndex]) minIndex=i;
+            if(nums[i]>nums[maxIndex]) maxIndex=i;
         }
-
-        // make sure minIndex is always before maxIndex
-        if (minIndex > maxIndex) {
-            int temp = minIndex;
-            minIndex = maxIndex;
-            maxIndex = temp;
-        }
-
-        // case 1: remove elements only from front
-        int deleteFromFront = maxIndex + 1;
-
-        // case 2: remove elements only from back
-        int deleteFromBack = n - minIndex;
-
-        // case 3: remove some from front (for min) and some from back (for max)
-        int deleteFromBoth = (minIndex + 1) + (n - maxIndex);
-
-        // return the smallest number of deletions among the three
-        return Math.min(deleteFromFront, Math.min(deleteFromBack, deleteFromBoth));
+        int left=Math.max(minIndex,maxIndex)+1;
+        int right=n-Math.min(minIndex,maxIndex);
+        int both=Math.min(minIndex,maxIndex)+1+n-Math.max(minIndex,maxIndex);
+        return Math.min(left,Math.min(right,both));
     }
 }
